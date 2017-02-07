da0a8304651d1d1dfdfdd02e0b55bf04dddfc391
Cédric <cedric@onekilopars.ec>
2017-02-07 08:23:26 +0100

    adding API sources to handle different servers

--- arcsecond/Arcsecond.swift
+++ arcsecond/Arcsecond.swift
@@ -7,45 +7,74 @@
 //
 
 import Foundation
-import RealmSwift
 import Siesta
+import Realm
+import RealmSwift
+
+public var defaultAPISource: APISource = .production
 
 // Auth
 
-public func login(username: String, password: String) -> Siesta.Request {
-    return ArcsecondService.sharedDefault.login(username: username, password: password)
+public func login(_ source: APISource = defaultAPISource, username: String, password: String) -> Siesta.Request {
+    return Arcsecond.service(source).login(username: username, password: password)
 }
 
-public func logout() -> Siesta.Request {
-    return ArcsecondService.sharedDefault.logout()
+public func logout(_ source: APISource = defaultAPISource) -> Siesta.Request {
+    return Arcsecond.service(source).logout()
 }
 
 // Singles
 
-public func observeObjectResource(withName name: String, observer: AnyObject, closure: @escaping Siesta.ResourceObserverClosure) {
-    return ArcsecondService.sharedDefault.observeObjectResource(withName: name, observer: observer, closure: closure)
+public func observeObjectResource(_ source: APISource = defaultAPISource,
+                                  withName name: String,
+                                  observer: AnyObject,
+                                  closure: @escaping Siesta.ResourceObserverClosure)
+{
+    return Arcsecond.service(source).observeObjectResource(withName: name, observer: observer, closure: closure)
 }
 
-public func observeExoplanetResource(withName name: String, observer: AnyObject, closure: @escaping Siesta.ResourceObserverClosure) {
-    return ArcsecondService.sharedDefault.observeExoplanetResource(withName: name, observer: observer, closure: closure)
+public func observeExoplanetResource(_ source: APISource = defaultAPISource,
+                                     withName name: String,
+                                     observer: AnyObject,
+                                     closure: @escaping Siesta.ResourceObserverClosure)
+{
+    return Arcsecond.service(source).observeExoplanetResource(withName: name, observer: observer, closure: closure)
 }
 
-public func observeObservingSiteResource(withUUID uuid: String, observer: AnyObject, closure: @escaping Siesta.ResourceObserverClosure) {
-    return ArcsecondService.sharedDefault.observeObservingSiteResource(withUUID: uuid, observer: observer, closure: closure)
+public func observeObservingSiteResource(_ source: APISource = defaultAPISource,
+                                         withUUID uuid: String,
+                                         observer: AnyObject,
+                                         closure: @escaping Siesta.ResourceObserverClosure)
+{
+    return Arcsecond.service(source).observeObservingSiteResource(withUUID: uuid, observer: observer, closure: closure)
 }
 
 // Collections
 
-public func observeObservingSitesResource(observer: AnyObject, closure: @escaping Siesta.ResourceObserverClosure) {
-    return ArcsecondService.sharedDefault.observeObservingSitesResource(observer: observer, closure: closure)
+public func observeObservingSitesResource(_ source: APISource = defaultAPISource,
+                                          observer: AnyObject,
+                                          closure: @escaping Siesta.ResourceObserverClosure)
+{
+    return Arcsecond.service(source).observeObservingSitesResource(observer: observer, closure: closure)
 }
 
+// Storage
 
-//public func objects() -> Results<[AstronomicalObject]> {
-//    return ArcsecondService.sharedDefault.objects()
-//}
-//
-//public func exoplanets() -> Results<Exoplanet> {
-//    return ArcsecondService.sharedDefault.exoplanets()
-//}
+public func save(_ source: APISource = defaultAPISource, object: Object) {
+    try! Arcsecond.service(source).save(object)
+}
+
+public func save(_ source: APISource = defaultAPISource, objects: [Object]) {
+    objects.forEach { try! Arcsecond.service(source).save($0) }
+}
+
+// Private
+
+private func service(_ source: APISource = defaultAPISource) -> ArcsecondService {
+    switch source {
+    case .localhost: return ArcsecondService.sharedLocalDefault
+    case .staging: return ArcsecondService.sharedStagingDefault
+    case .production: return ArcsecondService.sharedDefault
+    }
+}
 
